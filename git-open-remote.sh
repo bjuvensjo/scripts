cf7420b4f7da041716f13e5d6739bc060c5613d0
Magnus Bjuvensjö <magnus.bjuvensjo@evry.com>
2017-11-15 08:53:58 +0100

    Updated git-open-remote.sh

--- git-open-remote.sh
+++ git-open-remote.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-[ -z "$1" ] || cd "$1"
-clone_url="$(git remote get-url origin | sed -E "s/(.*\/\/).*@(.*)/\1\2/" )"
+if [ -z "$1" ]; then
+    git_dir="./.git"
+else
+    git_dir="$1/.git"
+fi
+
+clone_url="$(git --git-dir="$git_dir" remote get-url origin | sed -E "s/(.*\/\/).*@(.*)/\1\2/" )"
 open "$clone_url"
