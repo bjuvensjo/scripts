f5338f5e5d5a2375ba714a3cecfcfb09376d52ee
Magnus Bjuvensjö <magnus.bjuvensjo@evry.com>
2017-11-19 22:00:25 +0100

    Updated bb-has-branch.sh

--- bb-has-branch.sh
+++ bb-has-branch.sh
@@ -4,8 +4,7 @@ key=$1
 repo=$2
 branch=$3
 
-has_branch="$(curl -s -u $U:$P http://cuso.edb.se/stash/rest/api/1.0/projects/$key/repos/$repo/branches?filterText=$branch | grep "\"displayId\":\"$branch\"")"
-
-if [ -n "$has_branch" ]; then
+if [ -n "$(curl -s -u $U:$P http://cuso.edb.se/stash/rest/api/1.0/projects/$key/repos/$repo/branches?filterText=$branch | \
+   grep "\"displayId\":\"$branch\"")" ]; then
     echo "$branch"
 fi
