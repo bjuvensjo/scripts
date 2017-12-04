a83585eded09d0ce4f6446138eb563d7460c817e
Magnus Bjuvensjö <magnus.bjuvensjo@evry.com>
2017-12-04 11:09:06 +0100

    new file:   bb_api.py

---
+++ bb_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from base64 import encodebytes
+from json import loads
+from os import environ
+from urllib.request import Request, urlopen
+
+
+def call(uri, request_data=None, method="GET"):
+    api_url = "http://cuso.edb.se/stash/rest/api/1.0"
+    auth = f"{environ['U']}:{environ['P']}"
+    basic_auth_header = f"Basic {encodebytes(auth.encode()).decode('UTF-8').strip()}"
+    url = f"{api_url}/{uri}"
+
+    request = Request(url,
+                      request_data,
+                      {
+                          "Authorization": basic_auth_header,
+                          "Content-Type": "application/json"
+                      },
+                      method=method)
+
+    reponse_data = urlopen(request).read()
+    return loads(reponse_data.decode('UTF-8'))
