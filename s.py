6b5e9086e1e8773c0066f612c6f80107b82130d4
Magnus Bjuvensjö <magnus.bjuvensjo@evry.com>
2017-12-04 23:22:58 +0100

    Replaced s.groovy with s.py

---
+++ s.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from re import compile
+from sys import argv
+
+
+def get_cases(s):
+    split = compile('[A-Z]?[^A-Z]+').findall(s)
+    capital = [w.capitalize() for w in split]
+    lower = [w.lower() for w in split]
+    upper = [w.upper() for w in split]
+    return '\n'.join([
+        ''.join(split),
+        ''.join(capital),
+        ''.join(lower),
+        ''.join(upper),
+        '_'.join(lower),
+        '_'.join(upper),
+        '-'.join(lower),
+        '-'.join(upper)
+    ])
+
+
+if __name__ == "__main__":
+    print(get_cases(argv[1]))
