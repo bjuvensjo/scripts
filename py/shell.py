d04e620739e65ad50684a5c3c36689322dcf18ba
Magnus Bjuvensjö <magnus.bjuvensjo@evry.com>
2018-01-16 00:35:32 +0100

    Added bb_get_clone_projects.py

---
+++ py/shell.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+import logging
+from shlex import split
+from subprocess import Popen, PIPE, STDOUT
+
+log = logging.getLogger(__name__)
+
+
+def chunks(l, n):
+    """ Yield successive n-sized chunks from l. """
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+
+def call(script, args):
+    """
+    Call script with args.
+    :param script: The script
+    :param args: The args
+    :return: See run_command
+    """
+    cmd = [script] + args
+    return run_command(' '.join(cmd))
+
+
+def run_command(command, return_output=False, cwd=None):
+    """
+    Runs command.
+    :param command: The command
+    :param return_output: If True, returns output
+    :param cwd: The directory to run in
+    :return: return code and output (if return_output) as tuple
+    """
+
+    log.info('Running: %s', command)
+    process = Popen(split(command), stdout=PIPE, stderr=STDOUT, cwd=cwd)
+    the_output = []
+    while True:
+        output = process.stdout.readline().decode()
+        if output == '' and process.poll() is not None:
+            break
+        if output:
+            if return_output:
+                the_output.append(output.replace('\n', ''))
+            log.info(output.replace('\n', ''))
+    rc = process.poll()
+    if rc:
+        raise SystemError(command)
+
+    return (rc, ''.join(the_output)) if return_output else rc
+
+
+def run_commands(commands, max_processes=10, cwd=None):
+    """
+    Runs commands in parallel processes.
+    :param commands: The commands
+    :param max_processes: The max number of parallel processes
+    :param cwd: The directory to tun in
+    :return: process (yielded)
+    """
+    if len(commands) > max_processes:
+        for chunk in chunks(commands, max_processes):
+            yield from run_commands(chunk, max_processes=max_processes, cwd=cwd)
+    else:
+        for process in [Popen(split(command), stdout=PIPE, stderr=STDOUT, cwd=cwd) for command in commands]:
+            process.wait()
+            yield process
