0d5863c4ad6570c4c3b402541c846fcbed7b72ba
Magnus Bjuvensjö <magnus.bjuvensjo@evry.com>
2018-02-16 08:27:06 +0100

    Cleanup

--- py/bb_fork_repos_git.py
+++ py/bb_fork_repos_git.py
@@ -6,9 +6,9 @@ from multiprocessing.dummy import Pool
 from traceback import print_exc
 
 import bb_clone_repos
-import bb_create_repo
-import bb_set_default_branches
-import command_all
+from bb_create_repo import create_repo
+from bb_set_default_branches import set_repo_default_branch
+from command_all import get_work_dirs
 from shell import run_command
 
 
@@ -18,13 +18,13 @@ def fork_repo(fork_project, branches, git_dir):
             run_command('git checkout {}'.format(b), return_output=True, cwd=git_dir)
         rc, original_origin = run_command('git remote get-url origin', return_output=True, cwd=git_dir)
         repo = original_origin.split('/')[-1][:-4]
-        bb_create_repo.create_repo(fork_project, repo)
         new_origin = '/'.join(original_origin.split('/')[:-2]) + '/' + fork_project.lower() + '/' + repo + '.git'
+        create_repo(fork_project, repo)
         run_command('git remote set-url origin {}'.format(new_origin), return_output=True, cwd=git_dir)
         run_command('git remote prune origin', return_output=True, cwd=git_dir)
         for b in branches:
             run_command('git push -u origin {}'.format(b), return_output=True, cwd=git_dir)
-        bb_set_default_branches.set_repo_default_branch((fork_project, repo), branches[0])
+        set_repo_default_branch((fork_project, repo), branches[0])
 
         print('{}/{}: {}'.format(fork_project, repo, new_origin))
         return (fork_project, repo), new_origin
@@ -34,7 +34,7 @@ def fork_repo(fork_project, branches, git_dir):
 
 def fork_repos(fork_project, branches, work_dir='.', repos=None, projects=None, max_processes=10):
     bb_clone_repos.main(work_dir, projects, repos, branches[0])
-    git_dirs = command_all.get_work_dirs('.git/', work_dir)
+    git_dirs = get_work_dirs('.git/', work_dir)
     with Pool(processes=max_processes) as pool:
         pool.map(partial(fork_repo, fork_project, branches), git_dirs)
 
