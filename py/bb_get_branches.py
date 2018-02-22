7e881ea851b65e0623eb722d5dd571980aa5a696
Magnus Bjuvensjö <magnus.bjuvensjo@evry.com>
2018-02-22 09:44:42 +0100

    Updated branch scripts to support paged API

--- py/bb_get_branches.py
+++ py/bb_get_branches.py
@@ -1,25 +1,43 @@
 #!/usr/bin/env python3
+import itertools
 from multiprocessing.dummy import Pool
-
-from itertools import product
 
 from bb_api import call
 from bb_utils import get_repo_specs
 
 
-def get_repo_branches(spec, branch=''):
-    return spec, call('/rest/api/1.0/projects/{}/repos/{}/branches?filterText={}'.format(spec[0], spec[1], branch))
+def get_branch_page(spec, branch, limit, start):
+    response = call(
+        '/rest/api/1.0/projects/{}/repos/{}/branches?filterText={}&limit={}&start={}'.format(spec[0], spec[1], branch,
+                                                                                             limit, start))
+    return response['size'], response['values'], response['isLastPage'], response.get('nextPageStart', -1)
+
+
+def get_all_branches(spec, tag=''):
+    limit = 25
+    start = 0
+    is_last_page = False
+
+    while not is_last_page:
+        size, values, is_last_page, start = get_branch_page(spec, tag, limit, start)
+
+        if size:
+            for value in values:
+                yield spec, value
 
 
 def get_branches(repo_specs, branch='', max_processes=10):
     with Pool(processes=max_processes) as pool:
-        return pool.starmap(get_repo_branches, product(repo_specs, [branch]))
+        return itertools.chain.from_iterable(pool.map(lambda s: get_all_branches(s, branch), repo_specs))
 
 
-def main(branch='', dirs=None, repos=None, projects=None):
+def main(branch='', name=False, dirs=None, repos=None, projects=None):
     specs = get_repo_specs(dirs, repos, projects)
     for spec, response in get_branches(specs, branch):
-        print('{}/{}: {}'.format(spec[0], spec[1], [value['displayId'] for value in response['values']]))
+        if name:
+            print(response['displayId'])
+        else:
+            print('{}/{}: {}'.format(spec[0], spec[1], response['displayId']))
 
 
 if __name__ == '__main__':
@@ -29,6 +47,9 @@ if __name__ == '__main__':
     parser.add_argument('-b', '--branch',
                         help='Branch filter',
                         default='')
+    parser.add_argument('-n', '--name',
+                        help='Print only tag name',
+                        action='store_true')
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-d', '--dirs', nargs='*', default=['.'],
                        help='Git directories to extract repo information from')
@@ -38,4 +59,4 @@ if __name__ == '__main__':
                        help='Projects, e.g. key1 key2')
     args = parser.parse_args()
 
-    main(args.branch, args.dirs, args.repos, args.projects)
+    main(args.branch, args.name, args.dirs, args.repos, args.projects)
