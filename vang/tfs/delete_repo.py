c4202653103d8120911b8cdc0093512881427c14
Magnus Bjuvensjö <magnus.bjuvensjo@evry.com>
2018-10-08 17:28:55 +0200

    Added vang/tfs/delete_repo.py

---
+++ vang/tfs/delete_repo.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import argparse
+from sys import argv
+
+from vang.tfs.api import call
+
+
+def delete_repo(repo):
+    organisation, project, name = repo.split('/')
+    repo_id = call(
+        f'/{organisation}/{project}/_apis/git/repositories/{name}?api-version=3.2',
+        method='GET',
+    )['id']
+    return call(
+        f'/{organisation}/{project}/_apis/git/repositories/{repo_id}?api-version=3.2',
+        method='DELETE',
+    )
+
+
+def main(repo):
+    response = delete_repo(repo)
+    print(response)
+
+
+def parse_args(args):
+    parser = argparse.ArgumentParser(description='Delete TFS repository')
+    parser.add_argument(
+        'repo',
+        help='The TFS repository to delete, e.g. organisation/project/repo1')
+    return parser.parse_args(args)
+
+
+if __name__ == '__main__':
+    args = parse_args(argv[1:])
+    main(args.repo)
