a842afcb26aa51dbdc3b7017a871c770ef7bb62e
Magnus Bjuvensjö <magnus.bjuvensjo@evry.com>
2019-11-20 23:45:52 +0100

    modified:   open_remote.py	new file:   tests/test_open_remote.py

---
+++ vang/git/tests/test_open_remote.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+from unittest.mock import patch, call
+
+import pytest
+from pytest import raises
+
+from vang.git.open_remote import get_origin_remote_url, open_remote, parse_args
+
+
+@pytest.mark.parametrize("remote, expected", [
+    ('origin', 'https://github.com/origin/scripts.git'),
+    ('foo', 'https://github.com/foo/scripts.git'),
+    ('none', None),
+])
+@patch('vang.git.open_remote.run_command')
+def test_get_origin_remote_url(mock_command, remote, expected):
+    mock_command.return_value = (0,
+                                 '\n'.join(('origin	https://github.com/origin/scripts.git (fetch)',
+                                            'origin	https://github.com/origin/scripts.git (push)',
+                                            'foo	https://github.com/foo/scripts.git (fetch)',
+                                            'foo	https://github.com/foo/scripts.git (push)')))
+    assert expected == get_origin_remote_url('repo_dir', remote)
+
+
+@pytest.mark.parametrize("remote_url, expected", [
+    ('https://github.com/origin/scripts.git', [call(['open', 'https://github.com/origin/scripts.git'])]),
+    (None, []),
+])
+@patch('vang.git.open_remote.run')
+@patch('vang.git.open_remote.get_origin_remote_url')
+def test_open_remote(mock_get_origin_remote_url, mock_run, remote_url, expected):
+    mock_get_origin_remote_url.return_value = remote_url
+    open_remote('repo_dir', 'origin')
+    assert expected == mock_run.mock_calls
+
+
+@pytest.mark.parametrize("args", [
+    'foo',
+])
+def test_parse_args_raises(args):
+    with raises(SystemExit):
+        parse_args(args.split(' ') if args else args)
+
+
+@pytest.mark.parametrize("args, expected", [
+    ['', {
+        'repo_dir': '.',
+        'remote': 'origin'
+    }],
+    ['-d repo_dir -r remote', {
+        'repo_dir': 'repo_dir',
+        'remote': 'remote'
+    }],
+])
+def test_parse_args_valid(args, expected):
+    assert expected == parse_args(args.split(' ') if args else '').__dict__
