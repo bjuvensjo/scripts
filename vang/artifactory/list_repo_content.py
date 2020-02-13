01253306c8f8cc96c83dbce4dfbfe20c5ab39da8
Magnus Bjuvensjö <magnus.bjuvensjo@evry.com>
2020-02-13 13:34:58 +0100

    new file:   vang/artifactory/list_repo_content.py

---
+++ vang/artifactory/list_repo_content.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+from argparse import ArgumentParser
+from sys import argv
+
+from vang.artifactory import api
+
+
+def get_repo_content(repo_key):
+    return api.call(f'/api/storage/{repo_key}?list&deep=1&listFolders=1')
+
+
+def list_repo_content(repo_key, only_files=True):
+    result = get_repo_content(repo_key)
+    result = result['files']
+    if only_files:
+        result = [f['uri'] for f in result if not f['folder']]
+    return result
+
+
+def main(repo_key, only_files):
+    for f in list_repo_content(repo_key, only_files):
+        print(f)
+
+
+def parse_args(args):
+    parser = ArgumentParser(
+        description='List repository content')
+    parser.add_argument(
+        'repo_key',
+        help='Repository key, e.g. libs-release')
+    parser.add_argument('-f', '--only_files', help='Print only ordinary files, i.e. not directories',
+                        action='store_true')
+    return parser.parse_args(args)
+
+
+if __name__ == '__main__':  # pragma: no cover
+    main(**parse_args(argv[1:]).__dict__)
