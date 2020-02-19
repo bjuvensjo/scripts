fe2c7b82b1405b2da7acb190f84234b1da19cb6b
Magnus Bjuvensjö <magnus.bjuvensjo@evry.com>
2020-02-20 00:09:15 +0100

    modified:   vang/jenkins/get_jobs.py

--- vang/jenkins/get_jobs.py
+++ vang/jenkins/get_jobs.py
@@ -7,17 +7,25 @@ from vang.jenkins.api import call
 
 FAILURE = 'FAILURE'
 SUCCESS = 'SUCCESS'
+NOT_BUILT = 'NOT_BUILT'
+UNKNOWN = 'UNKNOWN'
 
 
 def map_color(color):
-    return SUCCESS if color == 'blue' else FAILURE
+    if color == 'blue':
+        return SUCCESS
+    if color == 'red':
+        return FAILURE
+    if color == 'notbuilt':
+        return NOT_BUILT
+    return UNKNOWN
 
 
-def get_jobs(statuses=(FAILURE, SUCCESS), only_names=False):
+def get_jobs(statuses=(FAILURE, SUCCESS, NOT_BUILT, UNKNOWN), only_names=False):
     jobs = call('/api/json')['jobs']
     return [
         job['name'] if only_names else job for job in jobs
-        if map_color(job['color']) in statuses
+        if map_color(job.get('color', None)) in statuses
     ]
 
 
@@ -38,16 +46,32 @@ def parse_args(args):
         action='store_true',
         help='Only success jobs',
     )
+    group.add_argument(
+        '-b',
+        '--only_not_built',
+        action='store_true',
+        help='Only not built jobs',
+    )
+    group.add_argument(
+        '-u',
+        '--only_unknown',
+        action='store_true',
+        help='Only unknown status jobs',
+    )
     return parser.parse_args(args)
 
 
-def main(only_failures, only_successes, only_names):
+def main(only_failures, only_successes, only_names, only_not_built, only_unknown):
     if only_failures:
         job_statuses = [FAILURE]
     elif only_successes:
         job_statuses = [SUCCESS]
+    elif only_not_built:
+        job_statuses = [NOT_BUILT]
+    elif only_unknown:
+        job_statuses = [UNKNOWN]
     else:
-        job_statuses = [FAILURE, SUCCESS]
+        job_statuses = [FAILURE, SUCCESS, NOT_BUILT, UNKNOWN]
 
     for a_job in get_jobs(job_statuses, only_names):
         print(a_job)
