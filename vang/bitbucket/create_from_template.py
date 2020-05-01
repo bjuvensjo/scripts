a0b757fb383296f72224048cdb7cf77d9aba6332
Magnus Bjuvensjö <magnus.bjuvensjo@evry.com>
2020-05-01 10:28:38 +0200

    modified:   vang/bitbucket/create_from_template.py

--- vang/bitbucket/create_from_template.py
+++ vang/bitbucket/create_from_template.py
@@ -11,17 +11,14 @@ from vang.pio.rsr import get_replace_function, rsr
 from vang.pio.shell import run_command
 
 
-def setup(project, repo, branch, dest_project, dest_repo, work_dir):
+def setup(project, repo, src_branch, dest_project, dest_repo, dest_branch, work_dir):
     clone_url = [c for _, p, r, c in get_clone_urls([project]) if r == repo][0]
-
     dest_repo_dir = f'{work_dir}/{dest_project}/{dest_repo}'
-    run_command(f'git clone -b {branch} {clone_url} {dest_repo_dir}')
-
+    run_command(f'git clone -b {src_branch} {clone_url} {dest_repo_dir}')
     run_command('rm -rf .git', return_output=True, cwd=dest_repo_dir)
     run_command('git init', return_output=True, cwd=dest_repo_dir)
-    if not branch == 'master':
-        run_command(
-            f'git checkout -b {branch}', return_output=True, cwd=dest_repo_dir)
+    if not dest_branch == 'master':
+        run_command(f'git checkout -b {dest_branch}', return_output=True, cwd=dest_repo_dir)
     return clone_url, dest_repo_dir
 
 
@@ -39,31 +36,27 @@ def update(repo, dest_repo, dest_repo_dir):
         rsr(old, new, [dest_repo_dir], get_replace_function(False))
 
 
-def create_and_push_to_dest_repo(branch,
-                                 dest_project,
+def create_and_push_to_dest_repo(dest_project,
                                  dest_repo,
+                                 dest_branch,
                                  dest_repo_dir,
                                  webhook=None):
-    dest_repo_origin = create_repo(dest_project,
-                                   dest_repo)['links']['clone'][0]['href']
+    dest_repo_origin = create_repo(dest_project, dest_repo)['links']['clone'][0]['href']
     if webhook:
         enable_repo_web_hook([dest_project, dest_repo], webhook)
-    run_command(
-        f'git remote add origin {dest_repo_origin}',
-        return_output=True,
-        cwd=dest_repo_dir)
-    run_command(
-        f'git push -u origin {branch}', return_output=True, cwd=dest_repo_dir)
-    set_repo_default_branch((dest_project, dest_repo), branch)
+    run_command(f'git remote add origin {dest_repo_origin}',
+                return_output=True,
+                cwd=dest_repo_dir)
+    run_command(f'git push -u origin {dest_branch}', return_output=True, cwd=dest_repo_dir)
+    set_repo_default_branch((dest_project, dest_repo), dest_branch)
     return dest_repo_origin
 
 
 def parse_args(args):
     parser = ArgumentParser(
         description='Create a new repo based on template repo.\nExample: '
-                    'create_from_template.vang PCS1806 foo PCS1806 bar -b '
-                    'develop -d . -w http://my-host:7777/wildcat/webhook/')
-
+                    'create_from_template PCS1806 foo PCS1806 bar -sb '
+                    'develop -db 1.0.x -d . -w http://my-host:7777/wildcat/webhook/')
     parser.add_argument(
         'src_project',
         help='The project from which to create the new repo (must exist)')
@@ -72,19 +65,26 @@ def parse_args(args):
         help='The repo from which to create the new repo (must exist)')
     parser.add_argument(
         'dest_project',
-        help='The project in which to create the new repo (must not exist)')
-    parser.add_argument('dest_repo', help='The new repo (must not exist)')
+        help='The project in which to create the new repo (must exist)')
     parser.add_argument(
-        '-b',
-        '--branch',
+        'dest_repo',
+        help='The new repo (must not exist)')
+    parser.add_argument(
+        '-sb',
+        '--src_branch',
         default='develop',
-        help='The branch to use and create, e.g. develop. '
+        help='The branch to use, e.g. develop')
+    parser.add_argument(
+        '-db',
+        '--dest_branch',
+        default='develop',
+        help='The branch to create, e.g. develop. '
              'It will be set as default branch on created repo')
     parser.add_argument(
         '-d',
         '--work_dir',
         default='.',
-        help='The directory to create local repo in')
+        help='The directory to create local repo in (directory work_dir/src_project/src_repo must not exists)')
     parser.add_argument(
         '-w',
         '--webhook',
@@ -93,18 +93,11 @@ def parse_args(args):
     return parser.parse_args(args)
 
 
-def main(src_project, src_repo, branch, dest_project, dest_repo, work_dir,
-         webhook):
-    clone_url, dest_repo_dir = setup(src_project, src_repo, branch,
-                                     dest_project,
-                                     dest_repo, work_dir)
-
+def main(src_project, src_repo, src_branch, dest_project, dest_repo, dest_branch, work_dir, webhook):
+    clone_url, dest_repo_dir = setup(src_project, src_repo, src_branch, dest_project, dest_repo, dest_branch, work_dir)
     update(src_repo, dest_repo, dest_repo_dir)
-
     commit_all(dest_repo_dir)
-
-    dest_repo_origin = create_and_push_to_dest_repo(
-        branch, dest_project, dest_repo, dest_repo_dir, webhook)
+    dest_repo_origin = create_and_push_to_dest_repo(dest_project, dest_repo, dest_branch, dest_repo_dir, webhook)
     print('Created', dest_repo_origin)
 
 
--- vang/bitbucket/tests/test_create_from_template.py
+++ vang/bitbucket/tests/test_create_from_template.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from unittest.mock import call, patch
 
+import pytest
 from pytest import raises
 
 from vang.bitbucket.create_from_template import commit_all
@@ -9,8 +10,6 @@ from vang.bitbucket.create_from_template import main
 from vang.bitbucket.create_from_template import parse_args
 from vang.bitbucket.create_from_template import setup as bitbucket_setup
 from vang.bitbucket.create_from_template import update
-
-import pytest
 
 
 @patch('vang.bitbucket.create_from_template.get_clone_urls', autospec=True)
@@ -21,60 +20,57 @@ def test_setup(mock_get_clone_urls):
         'repo',
         'clone_url',
     ]]
-    # branch 'branch'
+    # dest_branch = 'develop'
     with patch('vang.bitbucket.create_from_template.run_command'
                ) as mock_run_command:
         assert ('clone_url',
-                'work_dir/dest_project/dest_repo') == bitbucket_setup(
-                    'project', 'repo', 'branch', 'dest_project', 'dest_repo',
-                    'work_dir')
+                'work_dir/dest_project/dest_repo') == bitbucket_setup('project', 'repo', 'branch', 'dest_project',
+                                                                      'dest_repo', 'develop', 'work_dir')
         assert [
-            call('git clone -b branch clone_url work_dir/dest_project/dest_repo'
-                 ),
-            call(
-                'rm -rf .git',
-                cwd='work_dir/dest_project/dest_repo',
-                return_output=True),
-            call(
-                'git init',
-                cwd='work_dir/dest_project/dest_repo',
-                return_output=True),
-            call(
-                'git checkout -b branch',
-                cwd='work_dir/dest_project/dest_repo',
-                return_output=True),
-        ] == mock_run_command.mock_calls
-    # branch 'master'
+                   call('git clone -b branch clone_url work_dir/dest_project/dest_repo'),
+                   call(
+                       'rm -rf .git',
+                       cwd='work_dir/dest_project/dest_repo',
+                       return_output=True),
+                   call(
+                       'git init',
+                       cwd='work_dir/dest_project/dest_repo',
+                       return_output=True),
+                   call(
+                       'git checkout -b develop',
+                       cwd='work_dir/dest_project/dest_repo',
+                       return_output=True),
+               ] == mock_run_command.mock_calls
+    # dest_branch 'master'
     with patch('vang.bitbucket.create_from_template.run_command'
                ) as mock_run_command:
         assert ('clone_url',
-                'work_dir/dest_project/dest_repo') == bitbucket_setup(
-                    'project', 'repo', 'master', 'dest_project', 'dest_repo',
-                    'work_dir')
+                'work_dir/dest_project/dest_repo') == bitbucket_setup('project', 'repo', 'branch', 'dest_project',
+                                                                      'dest_repo', 'master', 'work_dir')
         assert [
-            call('git clone -b master clone_url work_dir/dest_project/dest_repo'
-                 ),
-            call(
-                'rm -rf .git',
-                cwd='work_dir/dest_project/dest_repo',
-                return_output=True),
-            call(
-                'git init',
-                cwd='work_dir/dest_project/dest_repo',
-                return_output=True),
-        ] == mock_run_command.mock_calls
+                   call('git clone -b branch clone_url work_dir/dest_project/dest_repo'
+                        ),
+                   call(
+                       'rm -rf .git',
+                       cwd='work_dir/dest_project/dest_repo',
+                       return_output=True),
+                   call(
+                       'git init',
+                       cwd='work_dir/dest_project/dest_repo',
+                       return_output=True),
+               ] == mock_run_command.mock_calls
 
 
 @patch('vang.bitbucket.create_from_template.run_command', autospec=True)
 def test_commit_all(mock_run_command):
     commit_all('repo_dir')
     assert [
-        call('git add --all', cwd='repo_dir', return_output=True),
-        call(
-            'git commit -m "Initial commit"',
-            cwd='repo_dir',
-            return_output=True),
-    ] == mock_run_command.mock_calls
+               call('git add --all', cwd='repo_dir', return_output=True),
+               call(
+                   'git commit -m "Initial commit"',
+                   cwd='repo_dir',
+                   return_output=True),
+           ] == mock_run_command.mock_calls
 
 
 @patch('vang.bitbucket.create_from_template.rsr', autospec=True)
@@ -86,8 +82,8 @@ def test_update(mock_get_zipped_cases, mock_get_replace_function, mock_rsr):
     mock_get_replace_function.return_value = 'get_replace_function'
     update('repo', 'dest_repo', 'dest_repo_dir')
     assert [
-        call('repo', 'dest_repo', ['dest_repo_dir'], 'get_replace_function')
-    ] == mock_rsr.mock_calls
+               call('repo', 'dest_repo', ['dest_repo_dir'], 'get_replace_function')
+           ] == mock_rsr.mock_calls
 
 
 @patch(
@@ -109,24 +105,24 @@ def test_create_and_push_to_dest_repo(mock_create_repo, mock_run_command,
                                       mock_set_repo_default_branch):
     # No webhook
     assert 'dest_repo_origin' == create_and_push_to_dest_repo(
-        'branch', 'dest_project', 'dest_repo', 'dest_repo_dir')
+        'dest_project', 'dest_repo', 'dest_branch', 'dest_repo_dir')
     assert [call('dest_project', 'dest_repo')] == mock_create_repo.mock_calls
     assert 0 == mock_enable_repo_web_hook.call_count
     assert [
-        call(
-            'git remote add origin dest_repo_origin',
-            cwd='dest_repo_dir',
-            return_output=True),
-        call(
-            'git push -u origin branch',
-            cwd='dest_repo_dir',
-            return_output=True)
-    ] == mock_run_command.mock_calls
+               call(
+                   'git remote add origin dest_repo_origin',
+                   cwd='dest_repo_dir',
+                   return_output=True),
+               call(
+                   'git push -u origin dest_branch',
+                   cwd='dest_repo_dir',
+                   return_output=True)
+           ] == mock_run_command.mock_calls
     assert [call(('dest_project', 'dest_repo'),
-                 'branch')] == mock_set_repo_default_branch.mock_calls
+                 'dest_branch')] == mock_set_repo_default_branch.mock_calls
     # webhook
     assert 'dest_repo_origin' == create_and_push_to_dest_repo(
-        'branch', 'dest_project', 'dest_repo', 'dest_repo_dir', 'webhook')
+        'dest_project', 'dest_repo', 'dest_branch', 'dest_repo_dir', 'webhook')
     assert [call(['dest_project', 'dest_repo'],
                  'webhook')] == mock_enable_repo_web_hook.mock_calls
 
@@ -144,18 +140,15 @@ def test_create_and_push_to_dest_repo(mock_create_repo, mock_run_command,
     return_value=['clone_url', 'dest_repo_dir'])
 def test_main(mock_setup, mock_update, mock_commit_all,
               mock_create_and_push_to_dest_repo, mock_print):
-    main('project', 'repo', 'branch', 'dest_project', 'dest_repo', 'work_dir',
-         'webhook')
+    main('project', 'repo', 'branch', 'dest_project', 'dest_repo', 'dest_branch', 'work_dir', 'webhook')
     assert [
-        call('project', 'repo', 'branch', 'dest_project', 'dest_repo',
-             'work_dir')
-    ] == mock_setup.mock_calls
-    assert [call('repo', 'dest_repo',
-                 'dest_repo_dir')] == mock_update.mock_calls
+               call('project', 'repo', 'branch', 'dest_project', 'dest_repo', 'dest_branch', 'work_dir')
+           ] == mock_setup.mock_calls
+    assert [call('repo', 'dest_repo', 'dest_repo_dir')] == mock_update.mock_calls
     assert [call('dest_repo_dir')] == mock_commit_all.mock_calls
     assert [
-        call('branch', 'dest_project', 'dest_repo', 'dest_repo_dir', 'webhook')
-    ] == mock_create_and_push_to_dest_repo.mock_calls
+               call('dest_project', 'dest_repo', 'dest_branch', 'dest_repo_dir', 'webhook')
+           ] == mock_create_and_push_to_dest_repo.mock_calls
     assert [call('Created', 'dest_repo_origin')] == mock_print.mock_calls
 
 
@@ -175,24 +168,26 @@ def test_parse_args_raises(args):
     [
         'src_project src_repo dest_project dest_repo',
         {
-            'branch': 'develop',
             'dest_project': 'dest_project',
             'dest_repo': 'dest_repo',
+            'dest_branch': 'develop',
             'work_dir': '.',
             'src_project': 'src_project',
             'src_repo': 'src_repo',
+            'src_branch': 'develop',
             'webhook': False
         }
     ],
     [
-        'src_project src_repo dest_project dest_repo -b b -d d -w w',
+        'src_project src_repo dest_project dest_repo -sb sb -db db -d d -w w',
         {
-            'branch': 'b',
             'dest_project': 'dest_project',
             'dest_repo': 'dest_repo',
+            'dest_branch': 'db',
             'work_dir': 'd',
             'src_project': 'src_project',
             'src_repo': 'src_repo',
+            'src_branch': 'sb',
             'webhook': 'w'
         }
     ],
