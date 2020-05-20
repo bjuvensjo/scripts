ff646325934d2311e09a0aef82c64e74437f60dd
Magnus Bjuvensjö <magnus.bjuvensjo@evry.com>
2020-05-20 15:19:12 +0200

    modified:   vang/artifactory/mirror.py

--- vang/artifactory/mirror.py
+++ vang/artifactory/mirror.py
@@ -2,14 +2,15 @@
 import hashlib
 from itertools import count
 from json import dumps
-from os import remove, makedirs
+from os import makedirs
 from os.path import exists
 
 from requests import get, put
 
 import vang.artifactory.mirror_config as config
-from vang.artifactory.diff_repos import diff_repos
+from vang.artifactory.diff_repos import diff, get_repo_content
 from vang.core.core import pmap_unordered
+from vang.misc.version import get_latest_snapshot, get_latest_rc, get_latest_fixed
 
 
 def read_file(file_path):  # pragma: no cover
@@ -30,17 +31,24 @@ def sha1sum(file_path):
 
 
 def download_file(output_file, url, sha1, retry=True):
-    if not exists(output_file) or sha1sum(output_file) != sha1:
-        with open(output_file, 'wb') as f:
-            f.write(get(url).content)
-
-    if sha1sum(output_file) == sha1:
-        return True
-    else:
-        if retry:
-            remove(output_file)
-            return download_file(output_file, url, sha1, False)
-        return False
+    if exists(output_file) and sha1sum(output_file) == sha1:
+        return True, 'Already downloaded'
+    with open(output_file, 'wb') as f:
+        result = get(url)
+        if result.status_code == 200:
+            f.write(result.content)
+            if sha1sum(output_file) == sha1:
+                return True, 'Downloaded and verified sha1'
+            else:
+                if retry:
+                    return download_file(output_file, url, sha1, False)
+                else:
+                    return False, 'Downloaded with bad sha1'
+        else:
+            if retry:
+                return download_file(output_file, url, sha1, False)
+            else:
+                return False, f'{result.status_code}: {result.content}'
 
 
 def get_checksum_headers(md5, sha1, sha256):
@@ -51,49 +59,96 @@ def get_checksum_headers(md5, sha1, sha256):
     }
 
 
-def mirror_file(repo_work_dir, from_repo_uri, artifact_spec, exclude_function, to_repo_uri, to_repo_username,
+def mirror_file(repo_work_dir, from_repo_uri, artifact_spec, to_repo_uri, to_repo_username,
                 to_repo_password):
     state = {
         'download_url': f'{from_repo_uri}{artifact_spec["uri"]}',
-        'excluded': exclude_function(from_repo_uri, artifact_spec)
     }
-    if state['excluded']:
-        state['result'] = True
-    else:
-        # download
-        d, f = artifact_spec['uri'].rsplit('/', maxsplit=1)
-        output_dir = f'{repo_work_dir}{d}'
-        makedirs(output_dir, exist_ok=True)
-        state['download_output_file'] = f'{repo_work_dir}{artifact_spec["uri"]}'
-        download_result = download_file(state['download_output_file'], state['download_url'], artifact_spec['sha1'],
-                                        True)
-        # TODO Return checksums from download and use in upload
-        state['download_result'] = download_result
+    # download
+    d, f = artifact_spec['uri'].rsplit('/', maxsplit=1)
+    output_dir = f'{repo_work_dir}{d}'
+    makedirs(output_dir, exist_ok=True)
+    state['download_output_file'] = f'{repo_work_dir}{artifact_spec["uri"]}'
+    download_result, download_message = download_file(state['download_output_file'], state['download_url'],
+                                                      artifact_spec['sha1'],
+                                                      True)
+    # TODO Return checksums from download and use in upload
+    state['download_result'] = download_result
+    state['download_message'] = download_message
 
-        # upload
-        if download_result:
-            content = read_file(state['download_output_file'])
-            md5, sha1, sha256 = get_checksums(content)
-            state['headers'] = {
-                **get_checksum_headers(md5, sha1, sha256),
-                'Content-Type': 'application/octet-stream'
-            }
-            state['upload_url'] = f'{to_repo_uri}{artifact_spec["uri"]}'
-            upload_result = put(state['upload_url'], data=content, headers=state['headers'],
-                                auth=(to_repo_username, to_repo_password))
-            state['upload_result'] = upload_result.status_code == 201
-            state['upload_status_code'] = upload_result.status_code
-            state['upload_response'] = upload_result.text
-            state['result'] = state['upload_result']
+    # upload
+    if download_result:
+        content = read_file(state['download_output_file'])
+        md5, sha1, sha256 = get_checksums(content)
+        state['headers'] = {
+            **get_checksum_headers(md5, sha1, sha256),
+            'Content-Type': 'application/octet-stream'
+        }
+        state['upload_url'] = f'{to_repo_uri}{artifact_spec["uri"]}'
+        upload_result = put(state['upload_url'], data=content, headers=state['headers'],
+                            auth=(to_repo_username, to_repo_password))
+        state['upload_result'] = upload_result.status_code == 201
+        state['upload_status_code'] = upload_result.status_code
+        state['upload_response'] = upload_result.text
+        state['result'] = state['upload_result']
 
     return state
+
+
+def not_folders(repo_files):
+    return filter(lambda r: not r['folder'], repo_files)
+
+
+def not_maven_metadata(repo_files):
+    return filter(lambda r: not r['uri'].endswith('maven-metadata.xml'), repo_files)
+
+
+def not_index(repo_files):
+    return filter(lambda r: '.index' not in r['uri'], repo_files)
+
+
+def latest_version(repo_files):
+    version_map = {}
+    for repo_file in repo_files:
+        uri = repo_file['uri']
+        group_and_artifact_id = '/'.join(uri.split('/')[:-2])
+        version = uri.split('/')[-2]
+        if group_and_artifact_id not in version_map:
+            version_map[group_and_artifact_id] = []
+        version_map[group_and_artifact_id].append(version)
+
+    latest_version_map = {}
+    for k, v in version_map.items():
+        try:
+            latest_version_map[k] = [get_latest_snapshot(v), get_latest_rc(v), get_latest_fixed(v)]
+        except ValueError:
+            latest_version_map[k] = v
+
+    latest_repo_files = []
+    for repo_file in repo_files:
+        uri = repo_file['uri']
+        group_and_artifact_id = '/'.join(uri.split('/')[:-2])
+        version = uri.split('/')[-2]
+        if version in latest_version_map[group_and_artifact_id]:
+            latest_repo_files.append(repo_file)
+
+    return latest_repo_files
+
+
+def filter_repo_files(repo_files, filters):
+    print(f'all: {len(repo_files)}')
+    for f in filters:
+        repo_files = f(repo_files)
+        repo_files = list(repo_files)
+        print(f'{f.__name__}: {len(repo_files)}')
+    return repo_files
 
 
 # TODO Maybe use "Last downloaded" to filter out even more
 # Note that files ending SNAPSHOT.* in from repo will get SNAPSHOT replaced
 # by a timestamp in to repo and thus will not be found in to_uri
 # on a rerun of this script. For now, nothing will be done about that.
-def mirror(max_processes=10):
+def mirror(filters, max_processes=10):
     for repo_key in config.repositories:
         if type(repo_key) is str:
             from_repo_key = repo_key
@@ -101,22 +156,19 @@ def mirror(max_processes=10):
         else:
             from_repo_key, to_repo_key = repo_key
 
-        files_not_in_to_repo, files_not_in_from_repo = diff_repos(
-            (
-                dict(config.from_artifactory_spec,
-                     artifactory_url=config.from_artifactory_spec['url'],
-                     repo_key=from_repo_key),
-                dict(config.to_artifactory_spec,
-                     artifactory_url=config.to_artifactory_spec['url'],
-                     repo_key=to_repo_key),
-            ),
-            only_keys=False)
+        from_repo_files = get_repo_content(from_repo_key, **config.from_artifactory_spec)['files']
+        filtered_from_repo_files = filter_repo_files(from_repo_files, filters)
+
+        to_repo_files = get_repo_content(to_repo_key, **config.to_artifactory_spec)['files']
+        files_not_in_to_repo, files_not_in_from_repo = diff(filtered_from_repo_files, to_repo_files, only_keys=False)
+        files_not_in_to_repo = list(files_not_in_to_repo)
+        print('Not in to repo:', len(files_not_in_to_repo))
 
         from_repo_uri = f'{config.from_artifactory_spec["url"]}/{from_repo_key}'
         to_repo_uri = f'{config.to_artifactory_spec["url"]}/{to_repo_key}'
         yield from pmap_unordered(
             lambda artifact_spec: mirror_file(f'{config.work_dir}/{from_repo_key}', from_repo_uri, artifact_spec,
-                                              config.is_excluded, to_repo_uri, config.to_artifactory_spec['username'],
+                                              to_repo_uri, config.to_artifactory_spec['username'],
                                               config.to_artifactory_spec['password']),
             files_not_in_to_repo,
             processes=max_processes)
@@ -124,7 +176,7 @@ def mirror(max_processes=10):
 
 if __name__ == '__main__':
     verbose = True
-    for n, m in zip(count(), mirror()):
+    for n, m in zip(count(), mirror(config.filters)):
         if verbose or not m['result']:
             print(n, dumps(m))
     print('Done!')
--- vang/artifactory/mirror_config.py
+++ vang/artifactory/mirror_config.py
@@ -1,11 +1,11 @@
-from os import environ
+from vang.artifactory.mirror import not_folders, not_index, not_maven_metadata, latest_version
 
 work_dir = './work_dir'
 # Spec of the artifactory instance to copy from
 from_artifactory_spec = {
-    'url': environ['ARTIFACTORY_REST_URL'],
-    'username': environ['ARTIFACTORY_USERNAME'],
-    'password': environ['ARTIFACTORY_PASSWORD'],
+    'url': '<from_artifactory_url>',
+    'username': '<username>',
+    'password': '<password>',
 }
 # Spec of the artifactory instance to copy to
 # In example below, by creating a copy of from_artifactory_spec with a different url
@@ -13,23 +13,8 @@ to_artifactory_spec = dict(from_artifactory_spec, url='<to_artifactory_url>')
 
 # Specify a pair (tuple) if the repo key to mirror to is not the same as the repo key to mirror from
 repositories = [
-    ('from_repo_key1', 'to_repo_key1'),
-    'from_and_to_repo_key1',
+    ('from_repo_key', 'to_repo_key'),
+    'from_and_to_repo_key'
 ]
 
-
-# This function is used to filter out files from mirroring
-def is_excluded(repo_uri, artifact_spec):
-    if artifact_spec['folder']:
-        return True
-    if artifact_spec['uri'].startswith('/.index/'):
-        return True
-    if artifact_spec['uri'].endswith('maven-metadata.xml'):
-        return True
-    if artifact_spec['uri'].endswith('tar'):
-        return True
-    if artifact_spec['uri'].endswith('war'):
-        return True
-    if artifact_spec['uri'].endswith('zip'):
-        return True
-    return False
+filters = (not_folders, not_index, not_maven_metadata, latest_version)
