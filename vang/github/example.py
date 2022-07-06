d246a1c8b2ac3cb8ad38e24b344fa0aeede06c63
Magnus Bjuvensjö <magnus.bjuvensjo@tietoevry.com>
2022-07-06 12:17:40 +0200

    Upgraded to python 3.10 and introduced Black for formatting

--- vang/github/example.py
+++ vang/github/example.py
@@ -8,36 +8,34 @@ from sys import argv
 
 from requests import post
 
+QueryArgs = namedtuple(
+    "QueryArgs", "endpoint token query variables operation_name", defaults=({}, None)
+)
 
-def query_github(endpoint, token, query, variables={}, operation_name=None):
-    json = {
-        'query': query
-    }
-    if variables:
-        json = json | {'variables': variables}
-    if operation_name:
-        json = json | {'operationName': operation_name}
+
+def query_github(args: QueryArgs):
+    json = {"query": args.query}
+    if args.variables:
+        json = json | {"variables": args.variables}
+    if args.operation_name:
+        json = json | {"operationName": args.operation_name}
     request = {
-        'url': endpoint,
-        'headers': {'Authorization': f'Bearer {token}'},
-        'json': json
+        "url": args.endpoint,
+        "headers": {"Authorization": f"Bearer {args.token}"},
+        "json": json,
     }
-
     r = post(**request)
     r.raise_for_status()
     return r.json()
 
 
 def get_login(endpoint, token):
-    query = 'query { viewer { login }}'
-    return query_github(endpoint, token, query)['data']['viewer']['login']
-
-
-Repository = namedtuple('Repository', 'name branches repo')
+    query = "query { viewer { login }}"
+    return query_github(QueryArgs(endpoint, token, query))["data"]["viewer"]["login"]
 
 
 def get_repositories(endpoint, token, login, first=100, with_message=True):
-    query = '''query Repos($login: String!, $first: Int!, $withMessage: Boolean!) {
+    query = """query Repos($login: String!, $first: Int!, $withMessage: Boolean!) {
       repositoryOwner(login: $login) {
         repositories(first: $first) {
           nodes {
@@ -62,67 +60,80 @@ def get_repositories(endpoint, token, login, first=100, with_message=True):
           }
         }
       }
-    }'''
-    variables = {'login': login, 'first': first, 'withMessage': with_message}
-    response = query_github(endpoint, token, query, variables)
-    nodes = response['data']['repositoryOwner']['repositories']['nodes']
+    }"""
+    variables = {"login": login, "first": first, "withMessage": with_message}
+    response = query_github(
+        QueryArgs(
+            endpoint,
+            token,
+            query,
+            variables,
+        )
+    )
+    nodes = response["data"]["repositoryOwner"]["repositories"]["nodes"]
 
     def get_refs(refs):
         result = []
-        for n in refs['nodes']:
-            r = {
-                'name': n['name'],
-                'commit': n['target']['oid']
-            } | ({'message': n['target']['message']} if 'message' in n['target'] else {})
+        for n in refs["nodes"]:
+            r = {"name": n["name"], "commit": n["target"]["oid"]} | (
+                {"message": n["target"]["message"]} if "message" in n["target"] else {}
+            )
             result.append(r)
         return result
 
-    return [{
-
-        'name': r['name'],
-        'branches': get_refs(r['branches']),
-        'tags': get_refs(r['tags']),
-    } for r in nodes]
+    return [
+        {
+            "name": r["name"],
+            "branches": get_refs(r["branches"]),
+            "tags": get_refs(r["tags"]),
+        }
+        for r in nodes
+    ]
 
 
 def parse_args(args):  # pragma: no cover
-    parser = argparse.ArgumentParser(
-        description='Query github')
+    parser = argparse.ArgumentParser(description="Query github")
     parser.add_argument(
-        '--endpoint',
-        '-e',
-        default='https://api.github.com/graphql',
-        help='The Github GraphQL endpoint')
+        "--endpoint",
+        "-e",
+        default="https://api.github.com/graphql",
+        help="The Github GraphQL endpoint",
+    )
     parser.add_argument(
-        '--token',
-        '-t',
-        default=environ.get('GITHUB_TOKEN', ''),
-        help='The Github authorisation token')
+        "--token",
+        "-t",
+        default=environ.get("GITHUB_TOKEN", ""),
+        help="The Github authorisation token",
+    )
     parser.add_argument(
-        '--login',
-        '-l',
+        "--login",
+        "-l",
         default=None,
-        help='The Github login')
+        help="The Github login",
+    )
     parser.add_argument(
-        '--first',
-        '-f',
+        "--first",
+        "-f",
         default=100,
-        help='The number of Github repos')
+        help="The number of Github repos",
+    )
     parser.add_argument(
-        '--with_message',
-        '-w',
+        "--with_message",
+        "-w",
         default=True,
-        help='Include commit messages')
-
+        help="Include commit messages",
+    )
     return parser.parse_args(args)
 
 
-def main(endpoint: str, token: str, login: str, first: int, with_message: bool) -> None:  # pragma: no cover
-    l = login or get_login(endpoint, token)
-    repositories = get_repositories(endpoint, token, l, first, with_message)
+def main(
+    endpoint: str, token: str, login: str, first: int, with_message: bool
+) -> None:  # pragma: no cover
+    login = login or get_login(endpoint, token)
+    repositories = get_repositories(endpoint, token, login, first, with_message)
     for r in repositories:
         pprint(r)
 
 
-if __name__ == '__main__':  # pragma: no cover
+if __name__ == "__main__":  # pragma: no cover
     main(**parse_args(argv[1:]).__dict__)
