9833101ad26fb025f75e50ae497194c937e9ae40
bjuvensjo <bjuvensjo@gmail.com>
2022-07-11 08:05:00 +0200

    Migrated build and distribution to pyproject.toml

--- distribute.sh
+++ distribute.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
-python setup.py sdist
-python setup.py bdist_wheel --universal
-twine upload dist/*
+# Remove old
+rm -rf dist
+
+# Make sure you have the latest version of PyPA’s build installed
+python3 -m pip install --upgrade build
+
+# Create source (tar.gz) and built (whl) distributions in dist
+python3 -m build
+
+# Install Twine
+python3 -m pip install --upgrade twine
+
+# Run Twine to upload all of the archives under dist
+#python3 -m twine upload dist/*
